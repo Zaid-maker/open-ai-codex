8811717a5bb9e3694494f69e9ec8fb958772b054
Zaid-maker <pzhafeez@gmail.com>
2022-12-26 15:11:58 +0500

    client and server fix

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const PORT = 3000;
+console.clear()
 
+const PORT = 3000;
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
@@ -8,7 +9,7 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config()
 
 const configuration = new Configuration({
-    apiKey: process.env.OPEN_API_KEY
+    apiKey: process.env.OPENAI_API_KEY
 })
 
 const openai = new OpenAIApi(configuration)
@@ -22,6 +23,29 @@ app.get('/', async (req, res) => {
     res.status(200).send({
         message: 'Hello from CodeX!'
     })
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt
+
+        const response = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0
+        })
+
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        });
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error || 'Something went wrong')
+    }
 })
 
 app.listen(PORT, () => {
