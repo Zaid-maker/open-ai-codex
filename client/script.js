59e868a610389213d27c3ea7ba2122b51a8c73a7
Zaid-maker <pzhafeez@gmail.com>
2022-12-27 17:38:53 +0500

    update server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:3000", {
+  const response = await fetch("https://api.devmirza.ml", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
